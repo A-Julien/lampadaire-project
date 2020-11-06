bb5193c4a94bdc801a33169cd74f3d2b50baa06d
A-Julien <julien.alaimo@gmail.com>
2020-11-06 22:04:56 +0100

    add docker login to resole jib push error in CI

--- docker_push.sh
+++ docker_push.sh
@@ -7,5 +7,8 @@ TAG=${PROJECT_VERSION}_${PARAM}
 #export docker tag env var for xml
 export TAG
 
-#push image
+#login to dockerhub
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+
+#build&push image
 ./mvnw package -X -Pprod verify -DskipTests jib:build
