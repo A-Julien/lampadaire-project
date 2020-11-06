b25f57e14d7cdfe568a10c2ae61c08e8d7eb4f51
A-Julien <julien.alaimo@gmail.com>
2020-11-06 13:18:36 +0100

    add docker config for recette and prod

--- docker_push.sh
+++ docker_push.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-cd ${HOME} || return
-echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+#build image
+./mvnw package -Pprod verify -DskipTests jib:dockerBuild
 
+#build docker tag
 PARAM="$@"
 TAG=${PROJECT_VERSION}_${PARAM}
 
-#build docker tag
-docker build -f src/main/docker/lampaderum-prod-springboot.dockerfile -t lampaderum_back .
-docker build -f src/main/docker/lampaderum-prod-nginx-angular.dockerfile -t lampaderum_frontend .
-
-docker images
-
-docker tag lampaderum_back $DOCKER_USERNAME/lampaderum_back:${TAG}
-docker tag lampaderum_frontend $DOCKER_USERNAME/lampaderum_frontend:${TAG}
-
 #push image
-docker push $DOCKER_USERNAME/lampaderum_back:${TAG}
-docker push $DOCKER_USERNAME/lampaderum_frontend:${TAG}
+cd ${HOME} || return
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+docker images
+docker tag lampaderum $DOCKER_USERNAME/lampaderum:${TAG}
+docker push $DOCKER_USERNAME/lampaderum:${TAG}
