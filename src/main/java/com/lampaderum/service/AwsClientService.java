b8642432ea6b725fe9b30004fd8259cb8c3b9059
A-Julien <julien.alaimo@gmail.com>
2020-12-11 19:05:41 +0100

    resolve error, add download file

--- src/main/java/com/lampaderum/service/AwsClientService.java
+++ src/main/java/com/lampaderum/service/AwsClientService.java
@@ -1,27 +1,38 @@
 package com.lampaderum.service;
 
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+
+import com.amazonaws.services.s3.model.S3Object;
+
+
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
+
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 import java.util.Date;
 import java.util.Objects;
 
 @Service
 public class AwsClientService {
+
+    private final Logger logger = LoggerFactory.getLogger(AwsClientService.class);
 
     private AmazonS3 s3client;
 
@@ -41,27 +52,62 @@ public class AwsClientService {
             .withRegion(Regions.EU_WEST_3).build();
     }
 
+    public ByteArrayOutputStream downloadFile(String keyName) {
+        try {
+            S3Object s3object = s3client.getObject(new GetObjectRequest(bucketName, keyName));
+
+            InputStream is = s3object.getObjectContent();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int len;
+            byte[] buffer = new byte[4096];
+            while ((len = is.read(buffer, 0, buffer.length)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+
+            return baos;
+        } catch (IOException ioe) {
+            logger.error("IOException: " + ioe.getMessage());
+        } catch (AmazonServiceException ase) {
+            logger.info("sCaught an AmazonServiceException from GET requests, rejected reasons:");
+            logger.info("Error Message:    " + ase.getMessage());
+            logger.info("HTTP Status Code: " + ase.getStatusCode());
+            logger.info("AWS Error Code:   " + ase.getErrorCode());
+            logger.info("Error Type:       " + ase.getErrorType());
+            logger.info("Request ID:       " + ase.getRequestId());
+            throw ase;
+        } catch (AmazonClientException ace) {
+            logger.info("Caught an AmazonClientException: ");
+            logger.info("Error Message: " + ace.getMessage());
+            throw ace;
+        }
+
+        return null;
+    }
+
     private File convertMultiPartToFile(MultipartFile file) throws IOException {
         File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
-        FileOutputStream fos = new FileOutputStream(convFile);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(convFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         fos.write(file.getBytes());
         fos.close();
         return convFile;
     }
 
-    /**
-     * Unique name genaration
-     *
-     * @param multiPart
-     * @return
-     */
     private String generateFileName(MultipartFile multiPart) {
         return new Date().getTime() + "-" + Objects.requireNonNull(multiPart.getOriginalFilename()).replace(" ", "_");
     }
 
     private void uploadFileTos3bucket(String fileName, File file) {
-        s3client.putObject(new PutObjectRequest(this.bucketName, fileName, file)
-            .withCannedAcl(CannedAccessControlList.Private));
+        try {
+            s3client.putObject(new PutObjectRequest(this.bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.Private));
+        } catch (SdkClientException e) {
+            e.printStackTrace();
+        }
     }
 
     public String uploadFile(MultipartFile multipartFile) {
@@ -69,7 +115,7 @@ public class AwsClientService {
         String fileUrl = "";
         try {
             File file = convertMultiPartToFile(multipartFile);
-            String fileName = generateFileName(multipartFile);
+            String fileName = multipartFile.getOriginalFilename();//generateFileName(multipartFile);
             fileUrl = this.endpointUrl + "/" + this.bucketName + "/" + fileName;
             uploadFileTos3bucket(fileName, file);
             file.delete();
@@ -81,7 +127,11 @@ public class AwsClientService {
 
     public String deleteFileFromS3Bucket(String fileUrl) {
         String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
-        s3client.deleteObject(new DeleteObjectRequest(this.bucketName + "/", fileName));
+        try {
+            s3client.deleteObject(new DeleteObjectRequest(this.bucketName + "/", fileName));
+        } catch (SdkClientException e) {
+            e.printStackTrace();
+        }
         return "Successfully deleted";
     }
 }
--- src/main/java/com/lampaderum/web/rest/AwsBucketController.java
+++ src/main/java/com/lampaderum/web/rest/AwsBucketController.java
@@ -4,8 +4,13 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.lampaderum.service.AwsClientService;
 import com.lampaderum.service.StreetlampService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.ResponseEntity;
+
+import java.io.ByteArrayOutputStream;
 
 @RestController
 @RequestMapping("/storage/")
@@ -26,4 +31,32 @@ public class AwsBucketController {
     public String deleteFile(@RequestPart(value = "url") String fileUrl) {
         return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
     }
+
+    @GetMapping("/api/file/{keyname}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String keyname) {
+        ByteArrayOutputStream downloadInputStream = this.amazonClient.downloadFile(keyname);
+
+        return ResponseEntity.ok()
+            .contentType(contentType(keyname))
+            .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + keyname + "\"")
+            .body(downloadInputStream.toByteArray());
+    }
+
+    private MediaType contentType(String keyname) {
+        String[] arr = keyname.split("\\.");
+        String type = arr[arr.length - 1];
+        switch (type) {
+            case "txt":
+                return MediaType.TEXT_PLAIN;
+            case "png":
+                return MediaType.IMAGE_PNG;
+            case "jpg":
+                return MediaType.IMAGE_JPEG;
+            case "pdf":
+                return MediaType.APPLICATION_PDF;
+            default:
+                return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
+
 }
