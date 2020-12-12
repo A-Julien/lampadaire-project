68ab3e0c358fb948730e5c76565cd496eef4ff96
A-Julien <julien.alaimo@gmail.com>
2020-12-12 22:35:08 +0100

    route to recap ok

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home.component';
 import { DetailPageComponent } from 'app/core/detail-page/detail-page.component';
 import { StreetlampResolve } from 'app/entities/streetlamp/streetlamp.route';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+import { PayRecapComponent } from 'app/core/pay-recap/pay-recap.component';
+import { User } from 'app/core/user/user.model';
 
 export const HOME_ROUTE: Routes = [
   {
@@ -20,6 +22,15 @@ export const HOME_ROUTE: Routes = [
     resolve: {
       streetlamp: StreetlampResolve,
     },
+    data: {
+      authorities: [],
+      pageTitle: 'home.title',
+    },
+    canActivate: [UserRouteAccessService],
+  },
+  {
+    path: 'recap',
+    component: PayRecapComponent,
     data: {
       authorities: [],
       pageTitle: 'home.title',
