28e167daffec514cbea1d3a91d69a821d1dfc253
SergentSnoopy <gary73@gmx.fr>
2020-12-14 11:24:18 +0100

    order page v2 without css

--- src/main/webapp/app/account/order/order.component.ts
+++ src/main/webapp/app/account/order/order.component.ts
@@ -4,6 +4,10 @@ import {SOrder} from 'app/shared/model/s-order.model';
 
 import {RoworderService} from 'app/entities/roworder/roworder.service';
 import {Roworder} from 'app/shared/model/roworder.model';
+
+import {StreetlampService} from 'app/entities/streetlamp/streetlamp.service';
+import {Streetlamp} from 'app/shared/model/streetlamp.model';
+
 import {ApplicationUser} from "../../shared/model/application-user.model";
 import {Creditcard} from "../../shared/model/creditcard.model";
 
@@ -12,7 +16,6 @@ import {User} from "../../core/user/user.model";
 import {AccountService} from "../../core/auth/account.service";
 import {ApplicationUserService} from "../../entities/application-user/application-user.service";
 import {UserService} from "../../core/user/user.service";
-import {CreditcardService} from "../../entities/creditcard/creditcard.service";
 
 @Component({
   selector: 'jhi-order',
@@ -25,10 +28,12 @@ export class OrderComponent implements OnInit {
   applicationUser: ApplicationUser | null = null;
   sorders: SOrder[];
   roworders:Roworder[];
+  lamps:Map<number,Streetlamp>;
   constructor(private accountService: AccountService, private ap:ApplicationUserService,private us:UserService,
-              private ss:SOrderService, private rs:RoworderService) {
+              private ss:SOrderService, private rs:RoworderService, private st :StreetlampService) {
     this.sorders=[];
     this.roworders=[];
+    this.lamps=new Map<number, Streetlamp>();
 
   }
 
@@ -52,34 +57,24 @@ export class OrderComponent implements OnInit {
 
                   for (let j=0;j<plip.body.length;j++){
                     this.roworders.push(new Roworder(plip.body[j].id,plip.body[j].price,plip.body[j].quantite,plip.body[j].streetlampId,plip.body[j].sorderId));
+                    this.st.find(plip.body[j].streetlampId).subscribe(plap=>{
+                      this.lamps.set(plap.body!.id!,new Streetlamp(plap.body!.id,plap.body!.libstreetlamp,plap.body!.modelestreetlamp,plap.body!.dureeviestreetlamp,plap.body!.uniteviestreetlamp,
+                        plap.body!.materiaustreetlamp,plap.body!.liblampe,plap.body!.pwlampe,plap.body!.formelampe,plap.body!.modelelampe,plap.body!.dureevielampe,plap.body!.unitevielampe,
+                        plap.body!.voltlampe,plap.body!.templampe,plap.body!.imagepathstreetlamp,plap.body!.stockstreetlamp,plap.body!.pricestreetlamp));
+                    });
                   }
                 });
 
                 this.sorders.push(new SOrder(plup.body[i].id,plup.body[i].datecommande,this.roworders,plup.body[i].applicationUserId));
               }
-              this.writreorder();
+
             });
 
         });
       });
+
+
+
     }
   }
-
-  writreorder() :void{
-    for (let i=0;i<this.sorders.length;i++)
-    {
-      document.getElementById("plop")!.innerHTML+="<div id='order"+i+"'>";
-      document.getElementById("order"+i)!.innerHTML+=this.sorders[i].datecommande!.calendar();
-      document.getElementById("plop")!.innerHTML+="<ul id='list"+i+"'>";
-      document.getElementById("plop")!.innerHTML+="<p>"+this.sorders[i].roworders!.length+"</p>";
-      for (let j=0;j<this.sorders[i].roworders!.length;j++)
-      {
-        document.getElementById("list"+i)!.innerHTML+="<li id='item"+j+"'>plop</li>";
-        document.getElementById("item"+j)!.innerHTML+=this.sorders[i].roworders![j].price +" "+this.sorders[i].roworders![j].quantite;
-        document.getElementById("item"+j)!.innerHTML+="plop";
-      }
-    }
-
-  }
-
 }
