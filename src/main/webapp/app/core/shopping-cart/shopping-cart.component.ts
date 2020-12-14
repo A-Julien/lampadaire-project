32a57461ab87f4534324f57cd3dee5a56ba77e67
SergentSnoopy <gary73@gmx.fr>
2020-12-14 16:08:22 +0100

    load shooping-cart with table cartpersi

--- src/main/webapp/app/core/shopping-cart/shopping-cart.component.ts
+++ src/main/webapp/app/core/shopping-cart/shopping-cart.component.ts
@@ -11,8 +11,14 @@ import {AccountService} from "../../core/auth/account.service";
 import {UserService} from "../../core/user/user.service";
 import { ApplicationUserService } from 'app/entities/application-user/application-user.service';
 import {ApplicationUser} from 'app/shared/model/application-user.model';
+
+import { RowcartService } from 'app/entities/rowcart/rowcart.service';
+import {Rowcart} from 'app/shared/model/rowcart.model';
+
 import {SOrder} from 'app/shared/model/s-order.model';
 import {Creditcard} from 'app/shared/model/creditcard.model';
+import {Streetlamp} from "app/shared/model/streetlamp.model";
+import {StreetlampService} from 'app/entities/streetlamp/streetlamp.service';
 
 @Component({
   selector: 'jhi-shopping-cart',
@@ -30,7 +36,8 @@ export class ShoppingCartComponent implements OnInit, OnDestroy {
 
   @Output() onOrderFinished: EventEmitter<boolean>;
 
-  constructor(private lampService: LampService,private accountService: AccountService, private ap:ApplicationUserService,private us:UserService,) {
+  constructor(private lampService: LampService,private accountService: AccountService, private ap:ApplicationUserService,private us:UserService,
+              private rc :RowcartService, private ss:StreetlampService) {
     this.total = 0;
     this.orderFinished = false;
     this.onOrderFinished = new EventEmitter<boolean>();
@@ -39,7 +46,7 @@ export class ShoppingCartComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.orders = new Cart();
 
-    if (Account) {
+    if (Account && this.lampService.ProductOrders.productOrders.length===0) {
       this.accountService.identity().subscribe(account => {
 
         this.Account=account;
@@ -50,7 +57,19 @@ export class ShoppingCartComponent implements OnInit, OnDestroy {
 
         this.ap.findbyuserid(this.User.id).subscribe((body: any)=> {
           this.applicationUser = new ApplicationUser(body.body.id, body.body.siret, body.body.userLogin, body.body.userId, new SOrder()[0], new Creditcard()[0], body.body.cartpersiId);
+          this.rc.findByCartpersiId(body.body.cartpersiId).subscribe((plop:any) => {
+            for (let i=0;i<plop.body.length;i++)
+            {
+              this.ss.find(plop.body[i]!.streetlampId!).subscribe((plip:any)=>{
 
+                this.orders.productOrders.push(new ProductOrder(new Streetlamp(plip.body!.id,plip.body!.libstreetlamp,plip.body!.modelestreetlamp,plip.body!.dureeviestreetlamp,plip.body!.uniteviestreetlamp,
+                  plip.body!.materiaustreetlamp,plip.body!.liblampe,plip.body!.pwlampe,plip.body!.formelampe,plip.body!.modelelampe,plip.body!.dureevielampe,plip.body!.unitevielampe,
+                  plip.body!.voltlampe,plip.body!.templampe,plip.body!.imagepathstreetlamp,plip.body!.stockstreetlamp,plip.body!.pricestreetlamp),plop.body[i].quantity));
+              });
+            }
+            this.lampService.ProductOrders=this.orders;
+
+          });
         });
 
       });
