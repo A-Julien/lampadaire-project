78cd281786c951065621a32514ddd83174a810b9
A-Julien <julien.alaimo@gmail.com>
2020-12-17 20:40:01 +0100

    resolve old merged error

--- src/main/java/com/lampaderum/config/Constants.java
+++ src/main/java/com/lampaderum/config/Constants.java
@@ -12,7 +12,7 @@ public final class Constants {
     public static final String DEFAULT_LANGUAGE = "fr";
     public static final String ANONYMOUS_USER = "anonymoususer";
 
-    public static final String SERV_IMG_URL = "https://lampaderum-img.s3.eu-west-3.amazonaws.com/";
+    public static final String SERV_IMG_URL = "https://lampaderum-img.s3.eu-west-3.amazonaws.com/cat_img/";
 
 
     private Constants() {
